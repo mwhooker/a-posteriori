80a319d900acc9a30737f98e7ce7b4a79b6d035f
Matthew Hooker <matt@cloudscaling.com>
2011-11-09 17:34:14 -0500

    experiment adding test runner

--- nose_plugins/tplugin.py
+++ nose_plugins/tplugin.py
@@ -1,3 +1,4 @@
+from nose.core import TextTestRunner
 from nose.plugins import Plugin
 
 class TestPlugin(Plugin):
@@ -6,3 +7,12 @@ class TestPlugin(Plugin):
     def wantMethod(self, method):
         if method.__name__ in ('pre', 'post'):
             return True
+
+    def prepareTestRunner(self, runner):
+        return TestTestRunner()
+
+class TestTestRunner(TextTestRunner):
+
+    def run(self, test):
+        print test
+        return super(TestTestRunner, self).run(test)
