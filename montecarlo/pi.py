ec7075c01595a4ad9ab77c09262877ebd7a6595d
Matthew Hooker <mwhooker@gmail.com>
2017-03-14 16:37:17 -0700

    multilock wip

--- montecarlo/pi.py
+++ montecarlo/pi.py
@@ -6,6 +6,9 @@ import math
 import signal
 import functools
 
+from contextlib import ContextDecorator
+from multiprocessing import Process, Lock
+
 
 def in_circle(x, y):
     return math.sqrt(x**2 + y**2) <= 1
@@ -15,22 +18,53 @@ def signal_handler(signal, frame, f):
     sys.exit(0)
 
 
+class Multilock(list, ContextDecorator):
+    def __enter__(self):
+        for lock in self:
+            lock.acquire()
+
+    def __exit__(self, *exc):
+        for lock in self:
+            lock.release()
+
+    def lock(self, *args, **kwargs):
+        lock = Lock(*args, **kwargs)
+        self.append(lock)
+        return lock
+
+
+class Simulator(object):
+    def __init__(self, locks):
+        self.locks = locks
+        self.inside = self.npoints = 0
+
+    def __call__(self, lock):
+        for _ in itertools.count(1):
+            with lock():
+                x = random.random()
+                y = random.random()
+                with lock:
+                    self.npoints += 1
+                    if in_circle(x, y):
+                        self.inside += 1
+
+    def print_results(self, exit):
+        with self.locks:
+            print
+            print("iterations: ", self.npoints)
+            print(4 * self.inside / self.npoints)
+            if exit:
+                sys.exit(0)
+    
 if __name__ == '__main__':
-    inside = npoints = 0
 
-    def print_results(exit):
-        print
-        print("iterations: ", npoints)
-        print(4 * inside / npoints)
-        if exit:
-            sys.exit(0)
 
-    signal.signal(signal.SIGINT, lambda *args: print_results(False))
-    signal.signal(signal.SIGQUIT, lambda *args: print_results(True))
+    max_workers = 1
+    locks = Multilock()
+    sim = Simulator(locks)
 
-    for i in itertools.count(1):
-        x = random.random()
-        y = random.random()
-        npoints = i
-        if in_circle(x, y):
-            inside += 1
+    for i in range(max_workers):
+        Process(target=sim, args=(locks.lock(),)).start()
+
+    signal.signal(signal.SIGINT, lambda *args: sim.print_results(False))
+    signal.signal(signal.SIGQUIT, lambda *args: sim.print_results(True))
